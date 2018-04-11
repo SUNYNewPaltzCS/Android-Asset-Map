a77360bf9588806a33c21d4c1b4281ee9605e703
Unknown <bmflynn34@gmail.com>
2018-04-10 23:24:40 -0400

    added example 2nd tier

---
+++ app/src/main/java/com/newburghmap/newburghmap/sub_education_activity.java
@@ -0,0 +1,24 @@
+package com.newburghmap.newburghmap;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class sub_education_activity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_education_activity);
+
+
+    }
+
+}
