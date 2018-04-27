ab5164b3824aada6fa44fcad2f84b8d8b7d83311
berryr1 <ralexberry@yahoo.com>
2018-04-27 14:13:20 -0400

    location fragment

--- app/src/main/java/com/newburghmap/newburghmap/type2_fragment.java
+++ app/src/main/java/com/newburghmap/newburghmap/type2_fragment.java
@@ -1,6 +1,8 @@
 package com.newburghmap.newburghmap;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -25,5 +27,65 @@ public class type2_fragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_type2_fragment, container, false);
     }
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        Activity act = getActivity();
+        act = (MapsActivity) act;
+        ((MapsActivity) act).locations("fin_lit");
+    }
+
+//    public ArrayList<String> locations(String subtype){
+//        InputStream credentialsJSON = getResources().openRawResource(getResources().getIdentifier("service_account_credentials", "raw", getPackageName()));
+//        try {
+//            credential = GoogleCredential
+//                    .fromStream(credentialsJSON, transport, jsonFactory)
+//                    .createScoped(Collections.singleton(FusiontablesScopes.FUSIONTABLES_READONLY));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        fclient = new Fusiontables.Builder(
+//                transport, jsonFactory, credential).setApplicationName("TestMap/1.0")
+//                .build();
+//        ArrayList<String> locations = new ArrayList<String>();
+//        try {
+//
+//            Sqlresponse result = null;
+//            String name, address;
+//            String q = "SELECT subtype, subtypeES, name, address FROM "+tableId;
+//
+//            result = query(tableId,q);
+//            List<List<Object>> rows = result.getRows();
+//
+//            for (List<Object> poi : rows) {
+//                if(!spanish){
+//                    String check = (String) poi.get(0);
+//                    if(subtype.equals(check)){
+//                        name = (String) poi.get(3);
+//                        address = (String) poi.get(4);
+//                        if(!locations.contains(name)){
+//                            locations.add("Name: " + name + "/n Address: " + address);
+//                        }
+//                    }
+//                }
+//                else{
+//                    String check = (String) poi.get(1);
+//                    if(subtype.equals(check)){
+//                        name = (String) poi.get(3);
+//                        address = (String) poi.get(4);
+//                        if(!locations.contains(name)){
+//                            locations.add("Name: " + name + "/n Address: " + address);
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return locations;
+//    }
 
 }
