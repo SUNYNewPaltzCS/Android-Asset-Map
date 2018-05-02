8491ea6b2b26191c6f91a08c8fca13b0656f0686
Unknown <bmflynn34@gmail.com>
2018-05-01 20:54:52 -0400

    Auto stash before merge of "master" and "BC/master"

--- app/src/main/java/com/newburghmap/newburghmap/type1_fragment.java
+++ app/src/main/java/com/newburghmap/newburghmap/type1_fragment.java
@@ -92,7 +92,8 @@ public class type1_fragment extends Fragment {
                 Activity act = getActivity();
                 act = (MapsActivity) act;
                 ((MapsActivity) act).clearMap();
-                ((MapsActivity) act).populateMapFromFusionTable(mainArrayList.get(groupPosition).get(childPosition));
+                //((MapsActivity) act).populateMapFromFusionTable(mainArrayList.get(groupPosition).get(childPosition));
+
                 return false;
             }
         });//mainArrayList.get(groupPosition).get(childPosition)
